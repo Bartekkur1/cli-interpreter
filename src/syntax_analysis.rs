b3ed92f35c6627d42ab056fbe7ed6278cf7f00a0
Bartek Kurpanik <bartekkur1@gmail.com>
2024-06-24 19:38:05 +0200

    Added analyze_syntax tests

--- src/syntax_analysis.rs
+++ src/syntax_analysis.rs
@@ -1,18 +1,12 @@
 use crate::lexer::{ Token, TokenVariant };
 
-pub fn analyze_syntax(mut tokens: Vec<Token>) -> Result<(), String> {
-    println!("Tokens len: {}", tokens.len());
-
-    // tokens.reverse();
-    let mut i = 0i32;
-
+pub fn analyze_syntax(tokens: Vec<Token>) -> Result<(), String> {
+    let mut i: i32 = 0;
     while i < (tokens.len() as i32) {
         let cursor = tokens.get(i as usize).unwrap();
-        println!("{}", cursor);
-
+        let prev_value_index = i - 1;
+        let next_value_index = i + 1;
         if cursor.variant() == TokenVariant::Operator {
-            let prev_value_index = i - 1;
-            let next_value_index = i + 1;
             if prev_value_index < 0 {
                 return Err(
                     format!("Syntax Error! Operator missing value at position {}", prev_value_index)
@@ -36,8 +30,88 @@ pub fn analyze_syntax(mut tokens: Vec<Token>) -> Result<(), String> {
                 );
             }
         }
+        if cursor.variant() == TokenVariant::Value {
+            let left_token_is_operator =
+                prev_value_index > 0 &&
+                tokens
+                    .get(prev_value_index as usize)
+                    .unwrap()
+                    .variant() == TokenVariant::Operator;
+            let right_token_is_operator =
+                next_value_index < (tokens.len() as i32) &&
+                tokens
+                    .get(next_value_index as usize)
+                    .unwrap()
+                    .variant() == TokenVariant::Operator;
+            if !left_token_is_operator && !right_token_is_operator {
+                return Err(
+                    format!("Syntax Error! Value token is missing operator at position {}", i)
+                );
+            }
+        }
 
         i += 1;
     }
     Ok(())
+}
+
+#[test]
+fn test_should_error_on_missing_next_operation_value() {
+    let mut tokens: Vec<Token> = Vec::new();
+    tokens.push(Token::Value { value: String::from("1") });
+    tokens.push(Token::Operator { value: '*' });
+
+    let syntax_result = analyze_syntax(tokens);
+    assert!(syntax_result.is_err());
+    assert_eq!(syntax_result.unwrap_err(), "Syntax Error! Operator missing value at position 2");
+}
+
+#[test]
+fn test_should_error_on_missing_next_operation_value_reverse() {
+    let mut tokens: Vec<Token> = Vec::new();
+    tokens.push(Token::Operator { value: '*' });
+    tokens.push(Token::Value { value: String::from("1") });
+
+    let syntax_result = analyze_syntax(tokens);
+    assert!(syntax_result.is_err());
+    assert_eq!(syntax_result.unwrap_err(), "Syntax Error! Operator missing value at position -1");
+}
+
+#[test]
+fn test_should_error_on_missing_prev_operation_value() {
+    let mut tokens: Vec<Token> = Vec::new();
+    tokens.push(Token::Operator { value: '*' });
+    tokens.push(Token::Value { value: String::from("1") });
+
+    let syntax_result = analyze_syntax(tokens);
+    assert!(syntax_result.is_err());
+    assert_eq!(syntax_result.unwrap_err(), "Syntax Error! Operator missing value at position -1");
+}
+
+#[test]
+fn test_should_error_on_invalid_command() {
+    let mut tokens: Vec<Token> = Vec::new();
+    tokens.push(Token::Value { value: String::from("1") });
+    tokens.push(Token::Operator { value: '*' });
+    tokens.push(Token::Operator { value: '*' });
+
+    let syntax_result = analyze_syntax(tokens);
+    assert!(syntax_result.is_err());
+    assert_eq!(
+        syntax_result.unwrap_err(),
+        "Syntax Error! Invalid token variant at 2, expected value"
+    );
+}
+
+#[test]
+fn test_should_fail_on_missing_operator() {
+    let mut tokens: Vec<Token> = Vec::new();
+    tokens.push(Token::Value { value: String::from("11") });
+
+    let syntax_result = analyze_syntax(tokens);
+    assert!(syntax_result.is_err());
+    assert_eq!(
+        syntax_result.unwrap_err(),
+        "Syntax Error! Value token is missing operator at position 0"
+    );
 }
